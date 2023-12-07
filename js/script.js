0deebb02c2b89cf742265fd45460014443bc93df
sandeep12345678910 <sandeeppok202@gmail.com>
2023-12-07 12:13:11 +0545

    box button

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-class User{
-    constructor(username){
-        this.username = username;
-    }
-    validate(){
-        if (this.username == null){
-            console.log("Username cannot be empty");
+// class User{
+//     constructor(username){
+//         this.username = username;
+//     }
+//     validate(){
+//         if (this.username == null){
+//             console.log("Username cannot be empty");
 
         
-        }
-        else { console.log("username received");
-    }
-}
-}
-let newUser = new User("hello hello");
-newUser.validate();
+//         }
+//         else { console.log("username received");
+//     }
+// }
+// }
+// let newUser = new User("hello hello");
+// newUser.validate();
 
-function display(){
-    var  username = document.getElementById("name")
-}
-jhgvcuyrc
+// function display(){
+//     var  username = document.getElementById("name")
+// }
+
+
+var Box = document.querySelector(".box"),
+ Show = document.querySelector(".show"),
+Hide = document.querySelector(".hide");
+
+Hide.addEventListener("click",function(){
+    Box.classList.add("hide-box");
+
+})
+
+Show.addEventListener("click",function(){
+    Box.classList.remove("hide-box");
+
+})
